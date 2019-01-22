b4aebff4948cd1cf94f1ebd2b5311422b14804d6
st20104172 <st20104172@s00049.internal.uwic.ac.uk>
2019-01-22 10:34:36 +0000

    initial commit

---
+++ helloworld/src/helloworld/AdvancedProg.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class AdvancedProg {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to advanced programming");
+		
+		System.out.println(50/3);
+	}
+}
---
+++ helloworld/src/helloworld/Person.java
@@ -0,0 +1,35 @@
+package helloworld;
+
+public class Person {
+
+	private String name;
+	private String firstName;
+	private String lastName;
+	private int age;
+	
+	public Person() {}
+	
+	public Person(String_name, int_age)
+	{
+		name = _name;
+		age = _age;
+	}
+	public Person(String fname, String lname, int _age)
+	{
+		firstName = fname;
+		lastName = lname;
+		age = _age;
+	}
+	
+	public String getLastName() {return lastName;}
+	public void setLastName(String lname) {lastName = lname;}
+	
+	public String getFirstName(){return firstName;}
+	public void setFirstName(String fname) {firstName = fname;}
+	
+	public String getName() {return name;}
+	public void setName(String _pname) {name = _pname;}
+	
+	public int getAge() {return age;}
+	public void setAge(int _page) {age = _page;}
+}
