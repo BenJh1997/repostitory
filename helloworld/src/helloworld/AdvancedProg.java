ab06107b0b0670a5c8621be1499cb527dc9ead1e
st20104172 <st20104172@s00053.internal.uwic.ac.uk>
2019-01-29 11:50:47 +0000

    Array Changes

--- helloworld/src/helloworld/AdvancedProg.java
+++ helloworld/src/helloworld/AdvancedProg.java
@@ -1,5 +1,7 @@
 package helloworld;
 
+import java.util.Scanner;
+
 public class AdvancedProg {
 	
 	public static void main(String[] args) {
@@ -7,5 +9,15 @@ public class AdvancedProg {
 		System.out.println("Welcome to advanced programming");
 		
 		System.out.println(50/3);
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("number of people: ");
+		int num_people = input.nextInt();
+		
+		String yourname;
+		int yourage;
+		
+		System.out.print("Your name: ");
+		String yourname = input.nextLine();
 	}
 }
